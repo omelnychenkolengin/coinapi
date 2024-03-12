46369e67651f44f5880dbc832ff753f148eea885
o.melnychenko <o.melnychenko>
2024-03-12 14:46:06 +0100

    First commit: Upload files

---
+++ includes/class-coinapi-integration-activator.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://example.org/
+ * @since      1.0.0
+ *
+ * @package    Coinapi_Integration
+ * @subpackage Coinapi_Integration/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Coinapi_Integration
+ * @subpackage Coinapi_Integration/includes
+ * @author     <info@example.org>
+ */
+class Coinapi_Integration_Activator {
+
+	/**
+	 *
+	 *
+	 * Create json file with coins data once plugin is activated.
+	 *
+	 * @since    1.0.0
+	 */
+
+
+	public function activate() {
+
+    }
+
+
+}
